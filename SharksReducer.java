f30c54fa1aaf69d5672db7d6d66b085cc58e6912
jjmalloy79 <joshuamalloy@hotmail.com>
2016-10-25 19:32:32 +0000

    revised needs to be able to output data

--- SharksReducer.java
+++ SharksReducer.java
@@ -22,8 +22,9 @@ public class SharksReducer extends Reducer<IntWritable, Text, IntWritable, Text>
     int unprovoked = 0;
 	String str = "";
     String time ="";
-	int [] attackTime = {0};
+	
 	int timer = 0;
+	ArrayList endingTime = new ArrayList ();
     for (Text t : values)
     {
 		//collects all the data
@@ -64,66 +65,105 @@ public class SharksReducer extends Reducer<IntWritable, Text, IntWritable, Text>
 
 	for(int i = 0; i < newTimes.size(); i ++)
 	{
-		timer = Integer.parseInt(newTimes.get(i).substring(0,2));
+		int temp = 0;
+		String token[] = newTimes.get(i).split("h");
+		timer = Integer.parseInt(token[0]);
 		if(timer >= 0 && timer <= 2)
 		{
 			int index = 0;
-			meme_list.set(index, meme_list.get(index)++);
+			temp = meme_list.get(0);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 2 && timer <= 4)
 		{
 			int index = 1;
-			meme_list.set(index, meme_list.get(index)++);
+			temp = meme_list.get(1);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 4 && timer <= 6)
 		{
 			int index = 2;
-			meme_list.set(index, meme_list.get(index)++);
+			temp = meme_list.get(2);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 6 && timer <= 8)
 		{
 			int index = 3;
-			meme_list.set(index, meme_list.get(index)++);
+			temp = meme_list.get(3);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 8 && timer <= 10)
 		{
 			int index = 4;
-			meme_list.set(index, meme_list.get(index)++);
+			temp = meme_list.get(4);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 10 && timer <= 12)
 		{
 			int index = 5;
-			meme_list.set(index, meme_list.get(index)++);
+			temp = meme_list.get(5);
+			temp++;
+			meme_list.set(index, temp);
+		}
+		else if(timer > 12 && timer <= 14)
+		{
+			int index = 6;
+			temp = meme_list.get(6);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 14 && timer <= 16)
 		{
-			int index = 6;
-			meme_list.set(index, meme_list.get(index)++);
+			int index = 7;
+			temp = meme_list.get(7);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 16 && timer <= 18)
 		{
-			int index = 7;
-			meme_list.set(index, meme_list.get(index)++);
+			int index = 8;
+			temp = meme_list.get(8);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 18 && timer <= 20)
 		{
-			int index = 8;
-			meme_list.set(index, meme_list.get(index)++);
+			int index = 9;
+			temp = meme_list.get(9);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 20 && timer <= 22)
 		{
-			int index = 9;
-			meme_list.set(index, meme_list.get(index)++);
+			int index = 10;
+			temp = meme_list.get(10);
+			temp++;
+			meme_list.set(index, temp);
 		}
 		else if(timer > 22 && timer <= 24)
 		{
-			int index = 10;
-			meme_list.set(index, meme_list.get(index)++);
+			int index = 11;
+			temp = meme_list.get(11);
+			temp++;
+			meme_list.set(index, temp);
 		}
 	}
-	//String results = newTimes + "," + result;
+	for(int j = 0; j < 12; j++)
+	{
+		if(meme_list.get(j) > 0)
+		{
+			String data = Integer.toString(j) + Integer.toString(meme_list.get(j));
+			endingTime.add(data);
+		}
+	}
+	String results = percentResults + "," + endingTime;
 
-    //context.write(key, new Text(result));
+    context.write(key, new Text(results));
   }
 
 }
